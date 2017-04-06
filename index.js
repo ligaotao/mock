f6c8eae1047cbe9916eaf559a462c3d6586142c0
ligaotao <774206981@qq.com>
2017-04-06 19:16:39 +0800

    增加mongodb配置中间件

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-
+const mongo = require('koa-mongo')
 // 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
 const cors = require('koa-cors')
@@ -9,6 +9,13 @@ const app = new Koa();
 const homeRouters = require('./routers/home')
 
 app.use(cors())
+app.use(mongo({
+  host: 'localhost',
+  port: 27017,
+  db: 'test',
+  max: 100,
+  min: 1,
+}))
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
--- routers/home.js
+++ routers/home.js
@@ -6,22 +6,20 @@ const MOCK = require('mockjs')
 
 homeRouters.get('/home', async (ctx, next) => {
   // get请求 用 ctx.request.query 即可获得
-  let data = {
-    'data|1-20': [{
-      'id|+1': 999,
-      'name': function () {return MOCK.Random.cname()},
-      'ico': function () { return MOCK.Random.image('100x100', MOCK.mock('@color'), 'img')}
-    }]
-  }
-  let obj = {
-    code: 200,
-    message: 'success',
-    data: MOCK.mock(data).data
-  }
-  ctx.response.body = obj
+  // const result = await ctx.mongo.db('test').collection('users').insert({name: 'lee'})
+  // const userId = result.ops[0]._id.toString();
+  ctx.response.body = await ctx.mongo.db('test').collection('users').find().toArray();
+  ctx.mongo.db('test').collection('users').remove({name: 'lee'})
+  // ctx.response.body = obj
 })
 .post('/home', koaBody, async (ctx, next) => {
-  ctx.response.body = ctx.request.body
+  const result = await ctx.mongo.db('test').collection('users').insert({name: 'lee'})
+  const userId = result.ops[0]._id.toString();
+  ctx.response.body = await ctx.mongo.db('test').collection('users').find().toArray();
+  ctx.mongo.db('test').collection('users').remove({
+    _id: mongo.ObjectId(userId)
+  });
+  
 })
 
 module.exports = homeRouters
